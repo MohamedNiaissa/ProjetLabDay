a623d1538b8c3faa71972c4ee5ea36cb41eda268
Leo <leojj.pro@gmail.com>
2021-12-16 12:09:15 +0100

    début form sell

--- src/elements/Forms/SellForm.js
+++ src/elements/Forms/SellForm.js
@@ -1,12 +1,24 @@
 import React from "react";
+import GroupForm from "./GroupForm";
 
+let TF_settings = [
+    {id: "FormGroup1", label: "Nom de l'objet", type: "text", hidden:"false"},
+    {id: "FormGroup2", label: "Matière de l'objet", type: "text", hidden:"false"},
+    {id: "FormGroup3", label: "Est-ce que cet objet est volumineux ?", type: "checkbox", hidden:"false"},
+    {id: "FormGroup4", label: "Location", type: "text", hidden:"true"}
+]
 
 class SellForm extends React.Component {
     render() {
+        console.log(TF_settings)
         return (
-            <p>Here's the Sell Form.</p>
+            <form id="ThrowForm">
+                { TF_settings.map((set) => <GroupForm {...this.props} set={set} key={set.id}/>) }
+
+                <button type="button">Chercher</button>
+            </form>
         )
-    } 
+    }  
 }
 
 export default SellForm;
