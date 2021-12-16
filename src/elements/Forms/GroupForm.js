185c60792b409eae3c15add8036cc6859819d0ad
Maghwyn <christopher.samson@edu.esiee-it.fr>
2021-12-16 18:25:06 +0100

    Added multiple events on each input of the form

--- src/elements/Forms/GroupForm.js
+++ src/elements/Forms/GroupForm.js
@@ -1,12 +1,38 @@
 import React from "react";
 
-class GroupForm extends React.Component{
-    render(){
-        console.log(this.props)
+class GroupForm extends React.Component {
+
+    componentDidMount() {
+        if(this.props.set.type === "text") {
+            if(this.input.style.display !== "none") this.input.setCustomValidity("Empty Field.");
+
+            this.input.addEventListener("input", function() {
+                !this.value.match(/[^a-zA-Z0-9]/) && (this.value.length > 2) ? this.setCustomValidity("") : this.setCustomValidity("Invalid field.");
+                if(this.validity.customError) console.log("Wrong character.")
+            })
+        } else {
+            this.input.addEventListener("input", function() {
+                const loc = document.getElementById("fpvzmxn");
+                loc.childNodes.forEach(element => {
+                    if(element.style.display === "none") {
+                        if(element.nodeName === "INPUT") element.setCustomValidity("Invalid Field.")
+                        element.style.display = "inline";
+                    }
+                    else {
+                        if(element.nodeName === "INPUT") element.setCustomValidity("");
+                        element.style.display = "none";
+                    }
+                })
+            })
+        }
+    }
+
+    render() {
         return (
-            <div id={this.props.set.id}>
-                <label hidden={this.props.set.visibility}>{this.props.set.label}</label>
-                <input type={this.props.set.type} hidden={this.props.set.visibility}></input>
+            <div id={this.props.set.id} className={this.props.set.className}>
+                <label style={{display:this.props.set.display}}>{this.props.set.label}</label>
+                <input type={this.props.set.type} style={{display:this.props.set.display}} 
+                        minLength = "2" maxLength="40" ref={e => (this.input = e)}></input>
             </div>
         )
     }
