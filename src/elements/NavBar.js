b3533d443f77ff49ed8d790906ee0972b6ab6939
Maghwyn <christopher.samson@edu.esiee-it.fr>
2021-12-16 22:13:57 +0100

    Fixed some warning about the img needing an alt property.

--- src/elements/NavBar.js
+++ src/elements/NavBar.js
@@ -3,7 +3,7 @@ import React from "react";
 const NavBar = () => (
     <div id="nav" style={{display:'flex'}}>
         <a>Application Name</a>
-        <img src=".../logo192.png"></img>
+        <img src="../logo192.png" alt=""></img>
         <a href="/">Menu</a>
     </div>
 )
