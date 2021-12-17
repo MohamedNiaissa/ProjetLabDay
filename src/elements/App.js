332ecca59985d9ac6cae1b831be6acdd3e157cd4
Mohamed <mohameddrame907@gmail.com>
2021-12-17 09:17:41 +0100

    Supprimer les log et test de la fonction  jquery

--- src/elements/App.js
+++ src/elements/App.js
@@ -17,20 +17,14 @@ class App extends React.Component {
           //  alert('ggg')
             const apiUrl = "https://geo.api.gouv.fr/communes?codePostal=";
             const format = '&format=json'; //on récupère les données en format JSON
-        
             let zipcode = $("#zipcode"); let city = ('#city'); let errorMessage=('#error-message');
-        
-        
-        
             $(zipcode).on('blur', function(){   
                 let code = $(this).val();  
                // console.log(code);
                 let url = apiUrl+code+format;
                 //console.log(url)
-        
                 fetch(url, {method: 'get'}).then(response => response.json()).then(results => {    
                     // console.log(results);
-        
                     $(city).find('option').remove(); 
                     if(results.length){  
                         $.each(results,function(key,value){     
@@ -59,8 +53,6 @@ class App extends React.Component {
         
     }
 
-    
-    
     render() {
 
         return (
