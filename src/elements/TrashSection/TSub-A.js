1d646b612be1da6893b2fda0974f9e5c0101b018
Maghwyn <christopher.samson@edu.esiee-it.fr>
2022-01-25 14:20:48 +0100

    Added a visualisation of what the page would do when an user chose to throw an object.

--- src/elements/TrashSection/TSub-A.js
+++ src/elements/TrashSection/TSub-A.js
@@ -1,11 +1,36 @@
 import React from "react";
 import { useLocation } from "react-router";
 
+function getTrash(loc) {
+    const material = loc.state.pMat;
+    let TProps = '';
+
+    switch(true) {
+        case ["bois"].includes(material):
+            return TProps = { name: "Poubelle Verte :", class: "T green" };
+        case ["metal", "plastique", "caoutchouc", "papier_carton"].includes(material):
+            return TProps = { name: "Poubelle Jaune :", class: "T yellow" };
+        case ["ceramique", "cuir", "textile"].includes(material):
+            return TProps = { name: "Poubelle Bleue :", class: "T blue" };
+        default: 
+            return TProps = { name: "Poubelle ? :", class: "T" };
+    }
+}
+
 const TSub_A = () => {
-    const location = useLocation();
+    let T = getTrash(useLocation());
+    console.log(T)
 
     return (
-        <p>I'll do the bin order here<br />With {location.state.pName} and {location.state.pMat}</p>
+        <fieldset id="trash">
+            <legend className="legend">Poubelle Ecologique :</legend>
+            <div className="trash-container">
+                <div className="color-trash">
+                    <p>{T.name}</p>
+                    <img className={T.class} src="../././Trash-Template.png"></img>
+                </div>
+            </div>
+        </fieldset>
     )
 }
 
