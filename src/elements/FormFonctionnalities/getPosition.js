60246008d84ea18cbb2416cb674106617e3ec4a9
Maghwyn <christopher.samson@edu.esiee-it.fr>
2022-01-25 20:33:18 +0100

    Removed the gps localisation for now as it serve no purpose. Passed the props so i can get the lat and long from a form.

--- src/elements/FormFonctionnalities/getPosition.js
+++ src/elements/FormFonctionnalities/getPosition.js
@@ -1,51 +1,29 @@
 import react from "react";
 import { MapContainer, TileLayer, Marker, Popup  } from 'react-leaflet'
 import Map from "./map";
-
 import React, { useRef, useState } from 'react';
+import { useLocation } from "react-router";
 
 
 const Getposition = () =>{
-    const location = Map();
+    // const location = Map();
+    const loc = useLocation();
+    const pointerLat = loc.state.cLat;
+    const pointerLong = loc.state.cLong;
 
     return(
-        
         <>
-        {/* {console.log("location " + location.coordinates.lat+ " " +location.coordinates.long)} */}
-        {location.loaded && !location.error && (
-
-        <MapContainer center={[location.coordinates.lat,location.coordinates.long]} zoom={13} scrollWheelZoom={false}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-         
-                <Marker position={[location.coordinates.lat,location.coordinates.long]}>
-                    <Popup>
-                        Here you are :-)
-                    </Popup>
-                </Marker>
-        
-        </MapContainer>
-        )}
-        {location.error &&  (
-
-            <MapContainer center={[48.889627,2.347078]} zoom={13} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                    <Marker position={[48.889627,2.347078]}>
+            <MapContainer center={[pointerLat,pointerLong]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                    <Marker position={[pointerLat,pointerLong]}>
                         <Popup>
-                            Default position
+                            Here you are :-)
                         </Popup>
                     </Marker>
-
             </MapContainer>
-        )}
         </>
     )
-    
 }
 
 export default Getposition;
