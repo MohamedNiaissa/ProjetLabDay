142ea1671f1ef22da71b97069a2c2e8944bf7700
Maghwyn <christopher.samson@edu.esiee-it.fr>
2022-01-25 01:58:35 +0100

    Init of the file where all the links will pass by.

---
+++ src/elements/ManageLinks.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+class ManageLinks extends React.Component {
+
+    getLink() {
+        if(this.props.link === "/jeter/poubelles-ecologiques") {
+            return (
+                <Link to={this.props.link}><button disabled={this.props.disabled}>Chercher</button></Link>
+            )
+        }else if (this.props.link === "/jeter/decharge") {
+            return (
+                <Link to={this.props.link}><button disabled={this.props.disabled}>Chercher</button></Link>
+            )
+        }
+    }
+
+    render() {
+        return (this.getLink())
+    }
+}
+
+export default ManageLinks;
