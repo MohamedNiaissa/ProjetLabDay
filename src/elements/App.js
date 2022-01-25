14b7f442aca40d606ad70be44ffeb7b4c8e8afc6
Maghwyn <christopher.samson@edu.esiee-it.fr>
2022-01-25 09:04:55 +0100

    Modified the app.js where NavLink is now inside the Router, it's to avoid a reload when we click on the home pagebutton. :wq

--- src/elements/App.js
+++ src/elements/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes as Switch, Route } from "react-router-dom";
-import NavBar from "./NavBar";
+import NavBar from "./Header";
 import CoreMenu from "./CoreMenu";
 import FunctionalitiesInfo from "./FunctionalitiesInfo";
 import SellForm from "./Forms/SellForm";
@@ -10,6 +10,7 @@ import ContactForm from "./Forms/ContactForm";
 import TForm from "./TrashSection/TForm";
 import TSub_A from "./TrashSection/TSub-A";
 import TSub_B from "./TrashSection/TSub-B";
+import Header from "./Header";
 // import $ from 'jquery'
 // import Givefonctionnality from './FormFonctionnalities/Givefonctionnality'
 
@@ -17,23 +18,20 @@ class App extends React.Component {
     render() {
         return (
             <>
-                <section id="NavBar">
-                    <NavBar />
-                </section>
+            <Router>
+                <Header />
                 <section id="Core">
-                    <Router>
-                        <Switch>
-                            <Route path="/" element={<><CoreMenu /><FunctionalitiesInfo /></>}/>
-                            <Route path="/Vendre" element={<><CoreMenu /><SellForm /></>}/>
-                            <Route path="/Donner" element={<><CoreMenu /><GiveForm /></>}/>
-                            <Route path="/Contact" element={<ContactForm />}/>
-
-                            <Route path="/jeter" element={<><CoreMenu /><TForm /></>}/>
-                            <Route path="/jeter/poubelles-ecologiques" element={<TSub_A />}/>
-                            <Route path="/jeter/decharge" element={<TSub_B />}/>
-                        </Switch>
-                    </Router>
+                    <Switch>
+                        <Route path="/home" element={<><CoreMenu /><FunctionalitiesInfo /></>}/>
+                        <Route path="/Vendre" element={<><CoreMenu /><SellForm /></>}/>
+                        <Route path="/Donner" element={<><CoreMenu /><GiveForm /></>}/>
+                        <Route path="/Contact" element={<ContactForm />}/>
+                        <Route path="/jeter" element={<><CoreMenu /><TForm /></>}/>
+                        <Route path="/jeter/poubelles-ecologiques" element={<TSub_A />}/>
+                        <Route path="/jeter/decharge" element={<TSub_B />}/>
+                    </Switch>
                 </section>
+            </Router>
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
             <script src="Givefonctionnality.js"></script>
             </>
--- src/elements/ManageLinks.js
+++ src/elements/ManageLinks.js
@@ -4,7 +4,11 @@ import { Link } from 'react-router-dom';
 class ManageLinks extends React.Component {
 
     getLink() {
-        if(this.props.link === "/jeter/poubelles-ecologiques") {
+        if(this.props.link === "/home") {
+            return (
+                <Link to={this.props.link}><button>Home</button></Link>
+            )
+        }else if(this.props.link === "/jeter/poubelles-ecologiques") {
             return (
                 <Link to={this.props.link}><button disabled={this.props.disabled}>Chercher</button></Link>
             )
