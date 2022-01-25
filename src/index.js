8a59aa62fc727b398cc02ca319e0f97c01cbc401
Maghwyn <christopher.samson@edu.esiee-it.fr>
2022-01-25 22:24:58 +0100

    From now on, only one css file will be loaded.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './elements/App';
 import "./css/style.css"
-import "./css/GiveForm.css"
-import "./css/mapstyle.css"
 
 ReactDOM.render(<App />, document.getElementById('root'))
