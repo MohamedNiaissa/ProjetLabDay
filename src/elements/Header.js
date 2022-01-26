1cf8dcc7f2bd65fb94712743f372a2af18f5f41d
Maghwyn <christopher.samson@edu.esiee-it.fr>
2022-01-26 20:51:15 +0100

    Added a way to fetch all images, and remade the design on the home page.

--- src/elements/Header.js
+++ src/elements/Header.js
@@ -1,12 +1,20 @@
 import React from "react";
 import ManageLinks from "./ManageLinks";
 
+function importAll(r) {
+    let images = {};
+    r.keys().map((item) => { images[item.replace('./', '')] = r(item); });
+    return images;
+}
+
 class Header extends React.Component {
+    imagesStatic = importAll(require.context('../img', false, /\.(png|jpe?g|svg)$/));
+
     render() {
         return (
             <section className="nav_container">
                 <div id="nav" className="nav">
-                    <img src='../NSLogoSVG.svg' alt="logo" className="app_logo" width="184px" height="56px"></img>
+                    <img src={this.imagesStatic['NSLogoSVG.svg']} alt="logo" className="app_logo" width="184px" height="56px"></img>
                     <ManageLinks link={"/home"}/>
                 </div>
             </section>
