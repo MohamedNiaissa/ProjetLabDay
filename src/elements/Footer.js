e3e4e1d1856dc6a60f6578a3cbe971b0fddd35a2
Maghwyn <christopher.samson@edu.esiee-it.fr>
2022-01-26 18:24:40 +0100

    Fixed the footer to the bottom of the window.

--- src/elements/Footer.js
+++ src/elements/Footer.js
@@ -2,9 +2,11 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Footer = () => (
-    <div class="footer">
-        <NavLink to="/Contact"><div className="contact">Contact us</div></NavLink>
-    </div>
+    <section className="footer_container"> 
+        <div className="footer">
+            <NavLink to="/contact"><div className="contact">Contact us</div></NavLink>
+        </div>
+    </section>
 )
 
 export default Footer;
