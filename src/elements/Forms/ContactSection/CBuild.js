2aced9529a622cc406353907036fd78e93497ee1
Maghwyn <christopher.samson@edu.esiee-it.fr>
2022-01-26 02:26:35 +0100

    Sligthly reworked the ContactForm

---
+++ src/elements/Forms/ContactSection/CBuild.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+class CBuild extends React.Component {
+
+    switchFormBlock() {
+        if(this.props.setAtt.inputForm.name === "textArea") {
+            return (
+            <div id={this.props.setAtt.id} className={this.props.setAtt.divForm.className} style={{display: this.props.setAtt.divForm.display}}>
+                <label>{this.props.setAtt.labelForm.text}</label>
+                <textarea className={this.props.setAtt.inputForm.className} name={this.props.setAtt.inputForm.name}
+                maxLength={1000} onChange={this.props.event}></textarea>
+            </div>
+            )
+        }else if(this.props.setAtt.inputForm.name === "topic") {
+            return (
+            <div id={this.props.setAtt.id} className={this.props.setAtt.divForm.className} style={{display: this.props.setAtt.divForm.display}}>
+                <label>{this.props.setAtt.labelForm.text}</label>
+                <select name={this.props.setAtt.inputForm.name} onChange={this.props.event}>
+                        <option value="default" style={{display: "none"}} defaultValue>...</option>
+                        <option value="Proposition d'amélioration">Proposition d'amélioration</option>
+                        <option value="Raport d'un bug">Raport d'un bug</option>
+                </select>
+            </div>
+            )
+        }else {
+            return (
+                <div id={this.props.setAtt.id} className={this.props.setAtt.divForm.className} style={{display: this.props.setAtt.divForm.display}}>
+                    <label>{this.props.setAtt.labelForm.text}</label>
+                    <input className={this.props.setAtt.inputForm.className} type={this.props.setAtt.inputForm.type} name={this.props.setAtt.inputForm.name}
+                        minLength = "2" maxLength="40" onChange={this.props.event}></input>
+                </div>
+                )
+        }
+    }
+
+    render() {
+        return (this.switchFormBlock())
+    }
+}
+
+export default CBuild;
---
+++ src/elements/Forms/ContactSection/CForm.js
@@ -0,0 +1,79 @@
+import React from "react";
+import CBuild from "./CBuild";
+import ManageLinks from "../../ManageLinks";
+
+class CForm extends React.Component {
+
+    constructor (props) {
+        super(props);
+        this.state = {
+          verify: {topic: false, email: null, textArea: null, validity: null },
+        }
+
+        this.form = {
+            email: null, topic: null, content: null, redirect: false,
+        }
+      }
+
+    handleUserInput = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.validateField(name, value, e);
+    }
+
+    validateField(fieldName, value, e) {
+        let validated = this.state.verify;
+
+        switch(fieldName) {
+            case "topic":
+                validated.topic = (value !== "default") ? true : false;
+                this.form.topic = value;
+                break;
+            case "email":
+                validated.email = value.match(/^(?=.{6,30}@)[0-9a-zA-Z]+(?:\.[0-9a-z]+)*@[a-z0-9-]{2,}(?:\.[a-z]{2,})+$/) ? true : false;
+                e.target.className = validated.email ? "validE" : "invalidE";
+                this.form.email = value;
+                break;
+            case "textArea":
+                validated.textArea = (!value.match(/[^a-zA-Z0-9"'`()\s\S]/) && value.length >= 10) ? true : false;
+                e.target.className = validated.textArea ? "validTA" : "invalidTA";
+                this.form.content = value;
+                break;
+            default: break;
+        }
+
+        validated.validity = this.state.verify.email && this.state.verify.topic && this.state.verify.textArea;
+        this.setState(this.state.verify = validated);
+        console.log(validated)
+      }
+
+    CF_settings = [
+        {   
+            id: "zfjfkzv",
+            divForm:   { className: "FormGroup", display: "inline" },
+            labelForm: { text: "Choissiez un topic :" },
+            inputForm: { className: '', type: "text", name: "topic" },
+        },{ 
+            id: "plfbszd",
+            divForm:   { className: "FormGroup", display: "inline" },
+            labelForm: { text: "Votre email :" },
+            inputForm: { className: '', type: "email", name: "email" },
+        },{
+            id: "ijconxa",
+            divForm:   { className: "FormGroup", display: "inline" },
+            labelForm: { text: "Que voulez vous nous dire ?" },
+            inputForm: { className: '', type: "text", name: "textArea" },
+        }
+    ]
+
+    render() {
+        return (
+            <form id="TrashForm">
+                { this.CF_settings.map((setAtt) => <CBuild {...this.props} setAtt={setAtt} key={setAtt.id} event={this.handleUserInput}/>) }
+                <ManageLinks link={"/contact/redirect"} form={this.form} disabled={!this.state.verify.validity}/>
+            </form>
+        )
+    } 
+}
+
+export default CForm;
