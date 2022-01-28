c4546fa628e6b36fbc7a3fd502a89089634078dd
Maghwyn <christopher.samson@edu.esiee-it.fr>
2022-01-28 14:01:27 +0100

    Added a passable burger menu, but it will be reworked later.

--- src/elements/Header.js
+++ src/elements/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ManageLinks from "./ManageLinks";
+import { Link } from 'react-router-dom';
 
 function importAll(r) {
     let images = {};
@@ -14,8 +14,21 @@ class Header extends React.Component {
         return (
             <section className="nav_container">
                 <div id="nav" className="nav">
-                    <img src={this.imagesStatic['NSLogoSVG.svg']} alt="logo" className="app_logo" width="184px" height="56px"></img>
-                    <ManageLinks link={"/home"}/>
+                    <Link to="/home">
+                        <img src={this.imagesStatic['NSLogoSVG.svg']} alt="logo" className="app_logo" width="184px" height="56px"></img>
+                    </Link>
+                    <div class="hamburger-menu">
+                        <input id="menu__toggle" type="checkbox" />
+                        <label class="menu__btn" for="menu__toggle">
+                            <span></span>
+                        </label>
+
+                        <div class="menu_img" />
+                        <ul class="menu__box">
+                        <li><Link to="/home"><a class="menu__item" href="#">Home</a></Link></li>
+                        <li><Link to="/contact"><a class="menu__item" href="#">Contact</a></Link></li>
+                        </ul>
+                    </div>
                 </div>
             </section>
         )
